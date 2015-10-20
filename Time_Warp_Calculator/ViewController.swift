bf43282edf2b328a401c2dac5a2184e9465e5d7e
javen123 <thisjaven@gmail.com>
2015-10-20 10:40:52 -0500

    first commit

---
+++ Time_Warp_Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time_Warp_Calculator
+//
+//  Created by Jim Aven on 10/20/15.
+//  Copyright © 2015 Jim Aven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
